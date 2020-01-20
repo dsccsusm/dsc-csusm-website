75302ae98d94897b1606c2d7c6b796f8954fb710
ACER <ace.g.fig@gmail.com>
2020-01-19 21:34:21 -0800

    Added flag page

---
+++ components/Google.js
@@ -0,0 +1,13 @@
+class Google extends React.Component {
+    render() { 
+        return(
+            <React.Fragment>
+                <span className="blue">G</span><span className="red">o</span>
+                <span className="yellow">o</span><span className="blue">g</span>
+                <span className="blue">l</span><span className="red">e</span>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Google
