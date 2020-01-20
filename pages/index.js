787df8e0be4bb22dad005852b668b789a3a8e4ab
ACER <ace.g.fig@gmail.com>
2020-01-20 06:01:51 -0800

    Added starting page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Navbar from "../components/Nav";
-import "../components/Google.js";
-
+import Google from "../components/Google.js";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,15 +8,14 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
 import "../styles/index.scss";
-import Google from "../components/Google.js";
 
 export default function Index() {
   return (
     <div>
       <Navbar />
       <Container className="mt-5">
-        <Row>
-          <Col>
+        <Row className="justify-content-center">
+          <Col className="align-self-center">
             <p className="h3"><strong>Developer Student Club @ CSUSM</strong></p>
             <p className="mt-3">Developer Student Clubs is a <Google/> Developers program for university students 
               to learn mobile and web development skills, design thinking skills and leadership 
@@ -25,7 +23,7 @@ export default function Index() {
             <Button variant="primary">Apply to be a member</Button>
           </Col>
           <Col>
-
+          <img src="/diversity.png" height="auto" width="512"/>
           </Col>
         </Row>
       </Container>
