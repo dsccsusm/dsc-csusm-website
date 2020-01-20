a3a42b76027489656b74538d5dbd4b2053bd77dd
Alex <me@alexandermgarcia.com>
2020-01-19 16:28:21 -0800

    Init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Nav";
+import "../styles/style.scss";
+
+export default function Index() {
+  return (
+    <div>
+      <Navbar />
+      <p className="example">Hello Next.js</p>
+    </div>
+  );
+}
