308d058cd3d84a761235f53fefe07a767a2f4ed7
ACER <ace.g.fig@gmail.com>
2020-01-19 21:34:01 -0800

    Added flag page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
 import Navbar from "../components/Nav";
+import "../components/Google.js";
+
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import "../styles/style.scss";
+import Button from "react-bootstrap/Button";
+
+import "../styles/index.scss";
+import Google from "../components/Google.js";
 
 export default function Index() {
   return (
     <div>
       <Navbar />
-      <Container>
-        <p className="example">Hello Next.js</p>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <p className="h3"><strong>Developer Student Club @ CSUSM</strong></p>
+            <p className="mt-3">Developer Student Clubs is a <Google/> Developers program for university students 
+              to learn mobile and web development skills, design thinking skills and leadership 
+              skills.</p>
+            <Button variant="primary">Apply to be a member</Button>
+          </Col>
+          <Col>
+
+          </Col>
+        </Row>
       </Container>
     </div>
   );
